4528b191b34eb02a55baca2e67f743e273b578e3
JeremyTheModernist <jeremyb1988@gmail.com>
2019-08-19 12:42:16 -0700

    finished responsive layout for project intro.

--- src/components/ProjectIntro/ProjectIntro.js
+++ src/components/ProjectIntro/ProjectIntro.js
@@ -4,7 +4,7 @@ function ProjectIntro(){
   return(
       <div class="projectintro">
         <h1 class="projectintro__header">
-          Trep Vessel Analytics
+          Tres Vessel Solutions
         </h1>
       <ul class="projectintro__overview">
         <ul class="projectintro__disciplines">
@@ -16,11 +16,11 @@ function ProjectIntro(){
         </ul>
         <li class="projectintro__challenge">
           <h3 class="challenge__header"> Challenge </h3>
-          Tres Solutions is a data analytics company supplying insightful metrics and feedback to vessel industries world wide. When Tres approached me, they we’re looking for a complete web redesign.
+          Tres Solutions is data analytics company supplying insightful metrics and feedback to vessel industries world wide. When Tres approached me, they we’re looking for a complete web redesign. The challenge was not only to create a brand image that differentiated Tres from their competitors, but more importantly, to create a visual and verbal language that would build trust with potential customers and communicate the value of Tres.
         </li>
         <li class="projectintro__solution">
           <h3 class="challenge__header"> Solution </h3>
-          My first step with Tres was to understand the unique value propositions of their company, and perform a competitive analysis. From there, I built a series of moodboards and initial design directions oriented around my findings.
+          My first step with Tres was to understand the unique value propositions of their company, and perform a competitive analysis. From there, I built a series of moodboards and initial design directions oriented around my findings. These initial directions would go onto define the larger rebrand and site architecture for Tres. The result was a comprehensive website detailing the unique offerings of Tres’ product and the value they offer customers.
         </li>
       </ul>
       </div>
