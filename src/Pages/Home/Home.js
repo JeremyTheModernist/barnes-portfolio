48ee7823562ec211068cbeba9838bcec8c9acfef
JeremyTheModernist <jeremyb1988@gmail.com>
2019-08-30 15:28:31 -0700

    fixed home page footer.

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -39,7 +39,7 @@ class Home extends React.Component{
 
             <XXLargeImage id={205} link="/graphicephemera"/>
 
-            <CTA home={true}/>
+            <CTA allprojects={true}/>
 
           </GridBox>
         </div>
